be0e3b56776297dccb023d56c59af8aa3017ab1d
Kririks <90604833+Kririks@users.noreply.github.com>
2023-03-28 15:50:35 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
 "use strict"
-let plusi = document.querySelector('#plusi');
-let plus_image = document.querySelector('#plus_image');
+let plusi_staj = document.querySelector('#plusi_staj');
+let plus_image_staj = document.querySelector('#plus_image_staj');
 
-plusi.addEventListener('mouseenter', function()
+plusi_staj.addEventListener('click', func)
+
+function func() { 
+    plus_image_staj.classList.toggle('active');
+    this.removeEventListener('click', func); 
+};
+
+let plusi_mesto = document.querySelector('#plusi_mesto');
+let plus_image_mesto = document.querySelector('#plus_image_mesto');
+
+plusi_mesto.addEventListener('mouseenter', function()
 {
- plus_image.classList.toggle('active');   
+    plus_image_mesto.classList.add('active'); 
+    plus_image_mesto.removeEventListener('click', func);
 });
