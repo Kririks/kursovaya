c5b11cdadf3d4d70e06632e4dc79487a19b1c835
Kririks <90604833+Kririks@users.noreply.github.com>
2023-05-12 07:10:04 +0300

    Add files via upload

---
+++ kursovaya-main/scripts/main_page_scripts.js
@@ -0,0 +1,35 @@
+'use strict'
+let button1 = document.querySelector('#usluga_name1');
+let elem1 = document.querySelector('#usluga_info1');
+
+button1.addEventListener('click', function(){
+    elem1.classList.toggle('active');
+});
+
+let button2 = document.querySelector('#usluga_name2');
+let elem2 = document.querySelector('#usluga_info2');
+
+button2.addEventListener('click', function(){
+    elem2.classList.toggle('active');
+});
+
+let button3 = document.querySelector('#usluga_name3');
+let elem3 = document.querySelector('#usluga_info3');
+
+button3.addEventListener('click', function(){
+    elem3.classList.toggle('active');
+});
+
+let button4 = document.querySelector('#usluga_name4');
+let elem4 = document.querySelector('#usluga_info4');
+
+button4.addEventListener('click', function(){
+    elem4.classList.toggle('active');
+});
+
+let button5 = document.querySelector('#usluga_name5');
+let elem5 = document.querySelector('#usluga_info5');
+
+button5.addEventListener('click', function(){
+    elem5.classList.toggle('active');
+});
